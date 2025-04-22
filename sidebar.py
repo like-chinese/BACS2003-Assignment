6384aae5de86c50a6af2f6690a487184ed1189a0
like-chinese <89380326+like-chinese@users.noreply.github.com>
2025-04-22 10:07:03 +0800

    Create sidebar.py

---
+++ sidebar.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def render_sidebar():
+    st.sidebar.title("🔀 切换模型页面")
+    st.sidebar.markdown("[🎯 Gradient Boosting (Doo Wei Jie)](app.py)")
+    st.sidebar.markdown("[🌲 XGBoost (Group Member 1)](app1.py)")
+    st.sidebar.markdown("[🌟 Random Forest (Group Member 2)](app2.py)")

dcf04d98ac54aecda6c61be1cdd7498657294c17
like-chinese <89380326+like-chinese@users.noreply.github.com>
2025-04-22 10:08:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import joblib
 import pandas as pd
+from sidebar import render_sidebar
+render_sidebar()
 
 # Load the trained model
 model = joblib.load("student_score_model.pkl")
